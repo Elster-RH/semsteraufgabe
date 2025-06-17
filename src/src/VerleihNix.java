06355f089cdd6d95e80c75e5bc2edcee1bd1ac64
Daniel <steiningerdan@gmx.de>
2025-06-18 01:10:28 +0200

    fenster angelegt

---
+++ src/src/VerleihNix.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class VerleihNix extends JFrame {
+
+    public VerleihNix() {
+
+        setTitle("VerleihNix");
+        setSize(600, 600);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+    }
+}
