dbc9743b74cd302c20b5c215570b4ac111a8f5a0
Daniel <steiningerdan@gmx.de>
2025-06-18 01:14:21 +0200

    adresse in student initialisiert

--- src/src/TestalleProgramme.java
+++ src/src/TestalleProgramme.java
@@ -6,7 +6,7 @@ public class TestalleProgramme {
         Address A1 = new Address(" BaumStr.", "13 f", 55672, "Auxburg");
         Address A2 = new Address(" AllehausStr.", "1", 58670, "Auxburg");
         Address A3 = new Address(" HausStr.", "122", 02670, "Friedburg");
-        Student S1 = new Student( "www.TimHuber@gmx.de", "Tim","Huber", A1, "+49 111 235 874" );
+        Student S1 = new Student( "www.TimHuber@gmx.de", "Tim","Huber", (new Address(" BaumStr.", "13 f", 55672, "Auxburg")) , "+49 111 235 874" );
         Student S2 = new Student( "www.TimSchlaeffer@gmx.de", "Tim","Hagen", A2, "+49 452 235 874" );
         Student S3 = new Student( "www.Faeffer@gmail.de", "Otto","Kullmn", A3, "+0821 775 772" );
 
