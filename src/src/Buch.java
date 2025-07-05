d363eef58e21217bc71ec5a41966b95918aabde8
R-Mr-M <robinc.matten@gmail.com>
2025-07-05 17:09:19 +0200

    buch

--- src/src/Buch.java
+++ src/src/Buch.java
@@ -1,10 +1,6 @@
 import java.util.ArrayList;
 
-
-
 public class Buch extends Gegenstaende {
-
-    private String bezeichnung;
 
     int buchid;
     private static int aktuelleBuchId = 0;
@@ -13,33 +9,15 @@ public class Buch extends Gegenstaende {
     private boolean outtofstock;
     private ArrayList<copyBook> amountCopys;
 
-
-
-  public Buch(int id, String kommentar, String bezeichnung, String modNumber,ArrayList amountCopys) throws RentalSystemException.EmptyField {
+  public Buch(int id, String kommentar, String bezeichnung, String modNumber) throws RentalSystemException.EmptyField {
         super(id, kommentar,"buch");
-
-        setBezeichnung(bezeichnung);
-
-
         setBuchid();
+        setAmount(amount);
+        firstlent = true;
+        outtofstock = true;
+        amountCopys = new ArrayList<copyBook>();
+
   }
-   public void setBezeichnung(String bezeichnung) throws RentalSystemException.EmptyField {
-       if (bezeichnung == null || bezeichnung.isEmpty()) {
-           throw new RentalSystemException.EmptyField();
-       }
-      this.bezeichnung = bezeichnung;
-    }
-
-    public String getBezeichnung() {
-        return bezeichnung;
-    }
-
-
-
-
-
-
-
 
     public static void setaktuelleBuchId(int ausgeleseneId) {
         if (aktuelleBuchId > ausgeleseneId) {
@@ -47,18 +25,80 @@ public class Buch extends Gegenstaende {
 
         }
     }
-    public static int getaktuelleBuchId(){
-        return aktuelleBuchId;
+    public void addCopy(copyBook copyBook) throws RentalSystemException {
+        if(amountCopys.size() == amount){
+            throw new RentalSystemException("Das Lager hat keine nicht eingetragenen Kopien des Buches mehr");
+        }
+      while (amountCopys.size() < amount) {
+          amountCopys.add(copyBook);
+        }
     }
 
+
+//setter
     public void setBuchid() {
         this.buchid = aktuelleBuchId;
         aktuelleBuchId++;
     }
+    public void setAmount(int amount) {
+      this.amount = amount;
+    }
 
+    public void setFirstlent(boolean firstlent) {
+      this.firstlent = firstlent;
+    }
+
+    public void setOuttofstock(boolean outtofstock) {
+      this.outtofstock = outtofstock;
+    }
+
+
+//getter
     public int getBuchid() {
         return buchid;
     }
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+    public static int getaktuelleBuchId(){
+        return aktuelleBuchId;
+    }
+    public int getAmount() {
+      return amount;
+    }
+    public boolean isFirstlent() {
+      return firstlent;
+    }
+    public boolean isOuttofstock() {
+      return outtofstock;
+    }
+
+
+
+    //getter des Arrays
+    public String getAmountCopys() {
+      // die stringausgabe des Arrays
+        return "Array";
+    }
+    public copyBook.Condition  getconditionArray(int index) throws RentalSystemException {
+        if(index >= amountCopys.size()) {
+            return amountCopys.get(index).getCondition();
+        } else throw new RentalSystemException("nur " + amountCopys.size() + " Eingetragen");
+    }
+
+    public Student getstudentArray(int index) throws RentalSystemException {
+        if(index >= amountCopys.size()) {
+            return amountCopys.get(index).getStudent();
+        } else throw new RentalSystemException("nur " + amountCopys.size() + " Eingetragen");
+    }
+
+    public int getPfandArray(int index) throws RentalSystemException {
+        if(index >= amountCopys.size()) {
+            return amountCopys.get(index).getPfand();
+        } else throw new RentalSystemException("nur " + amountCopys.size() + " Eingetragen");
+    }
+
+
 
     @Override
     public String toString() {
--- src/src/copyBook.java
+++ src/src/copyBook.java
@@ -4,7 +4,6 @@ public class copyBook {
 
     private String modNumber;
     private Student student;
-    private final LocalDate lentDate = LocalDate.now();
     private LocalDate backDate;
     private LocalDate lentduration;
     private int pfand;
@@ -13,32 +12,38 @@ public class copyBook {
     }
     Condition condition;
 
-    public copyBook(String modNumber, Student student, LocalDate lentDate, int pfand) throws RentalSystemException{
+    private final LocalDate lentDate = LocalDate.now();
+    public copyBook(String modNumber, Student student, Condition condition) throws RentalSystemException{
         setModNumber(modNumber);
+        setStundent(student);
         setCondition(condition);
+
     }
 
-
-    public void addBackDate(int backDate) throws RentalSystemException {
-
-        LocalDate back = lentDate.plusDays(backDate);
-        this.backDate = back ;
+    public void setModNumber(String modNumber) throws RentalSystemException.EmptyField {
+        if (modNumber == null || modNumber.isEmpty()) {
+            throw new RentalSystemException.EmptyField();
+        }
+        this.modNumber = modNumber;
     }
+
     public void setLentduration(LocalDate lentduration) throws RentalSystemException {
         this.lentduration = lentduration;
     }
 
-    public void setPfand(int pfand) throws RentalSystemException {
+    public void setStundent(Student student) {
+        this.student = student;
+    }
 
+    public void setPfand(int pfand) throws RentalSystemException {
         if (pfand > 0) {
             this.pfand = pfand;
         } else {
             throw new RentalSystemException("Pfand zu klein");
         }
     }
-    public void setCondition(Condition condition)throws RentalSystemException.EmptyField
-    {
 
+    public void setCondition(Condition condition)throws RentalSystemException.EmptyField {
         if (condition == null) {
             throw new RentalSystemException.EmptyField();
         }
@@ -46,40 +51,26 @@ public class copyBook {
 
     }
 
-    public Condition getCondition() {
-        return condition;
+    public void addBackDate(int backDate) throws RentalSystemException {
+        LocalDate back = lentDate.plusDays(backDate);
+        this.backDate = back ;
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    public void setModNumber(String modNumber) throws RentalSystemException.EmptyField{
-        if (modNumber == null || modNumber.isEmpty()) {
-            throw new RentalSystemException.EmptyField();
-        }
-        this.modNumber = modNumber;
+    public Condition getCondition() {
+        return condition;
     }
 
     public String getModNumber() {
         return modNumber;
     }
+    public Student getStudent() {
+        return student;
+    }
 
-
+    public int getPfand() {
+        return pfand;
+    }
 }
+
+
+
